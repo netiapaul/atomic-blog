27ff536421c2213c12d75197e065daf0c9cdf596
Paul Netia <netiapaul23@gmail.com>
2024-01-29 12:58:06 +0300

    - added memo for performance management

--- src/postContext.js
+++ src/postContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -33,15 +33,27 @@ function PostProvider({ children }) {
   function handleClearPosts() {
     setPosts([]);
   }
+
+  const value = useMemo(() => {
+    return {
+      posts: searchedPosts,
+      onAddPost: handleAddPost,
+      onClearPosts: handleClearPosts,
+      searchQuery,
+      setSearchQuery,
+    };
+  }, [searchedPosts, searchQuery]);
+
   return (
     <PostContext.Provider
-      value={{
-        posts: searchedPosts,
-        onAddPost: handleAddPost,
-        onClearPosts: handleClearPosts,
-        searchQuery,
-        setSearchQuery,
-      }}
+      value={value}
+      // value={{
+      //   posts: searchedPosts,
+      //   onAddPost: handleAddPost,
+      //   onClearPosts: handleClearPosts,
+      //   searchQuery,
+      //   setSearchQuery,
+      // }}
     >
       {children}
     </PostContext.Provider>
